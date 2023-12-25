3bf2b3082ee9b33630a031cb6214b7c27f29d2d3
spm81 <sergiomatoz@hotmail.com>
2023-12-25 16:17:29 +0000

    Fix ENABLE_LCD_INVERT_OPTION

--- ui/fmradio.d
+++ ui/fmradio.d
@@ -1,5 +1,6 @@
 ui/fmradio.o: ui/fmradio.c ui/../app/fm.h \
- C:/GITHUB/compile/MCFW_UV-K5_Open_Source_Firmware/driver/keyboard.h \
+ c:/GITHUB/compile/MCFW_UV-K5_Open_Source_Firmware/driver/keyboard.h \
  ui/../driver/st7565.h ui/../external/printf/printf.h ui/../misc.h \
- ui/../settings.h ui/../radio.h ui/../dcs.h ui/fmradio.h ui/helper.h \
- ui/inputbox.h ui/ui.h
+ ui/../settings.h ui/../radio.h ui/../dcs.h \
+ c:/GITHUB/compile/MCFW_UV-K5_Open_Source_Firmware/helper/battery.h \
+ ui/fmradio.h ui/helper.h ui/inputbox.h ui/ui.h
